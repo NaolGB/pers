79ef1b0f97f14ddf5754cb9aee515265be072eb3
NaolGB <naolgbasaye@gmail.com>
2023-09-11 04:21:11 +0300

    pre-channels

--- hms/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import HMSProduct, HMSStockTransaction, HMSWarehouse
-
-admin.site.register(HMSProduct)
-admin.site.register(HMSStockTransaction)
-admin.site.register(HMSWarehouse)
-
