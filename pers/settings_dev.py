6c8829ca94ca3598590d69d0723f129d0b84f16c
NaolGB <naolgbasaye@gmail.com>
2023-09-17 13:34:21 +0100

    cleaned up for prod

--- pers/settings_dev.py
+++ pers/settings_dev.py
@@ -49,8 +49,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-
-                # 'user_management.context_processors.recent_actions',
             ],
         },
     },
